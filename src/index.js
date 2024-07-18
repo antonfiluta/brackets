3c4e8138ac2fd5278406e5af88a62d8b6d47edb9
antonfiluta <antonfiluta2.0@mail.ru>
2024-07-18 10:28:46 +0300

    feat: finish project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, Config) {
+  let symbols = [];
+   for (let conf of Config) {
+     symbols.push(conf[0] + conf[1])
+   }
+
+   function checkBool() {
+    for (let sym of symbols) {
+      if (str.includes(sym)) return true;
+    }
+    return false
+   }
+
+   while(checkBool()) {
+    for (let sym of symbols) {
+      if (str.includes(sym)) {
+        let arrStr = str.split('');
+        arrStr.splice(str.indexOf(sym), 2);
+        str = arrStr.join('');
+      }
+    }
+   }
+
+   return !str[0]
 }
